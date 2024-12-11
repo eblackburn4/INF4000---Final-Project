632a852fa62308e005aa40e68937a92ddc783436
eblackburn4 <eblackburn4@sheffield.ac.uk>
2024-12-11 15:38:02 +0000

    created script file

---
+++ Data visualisation code.R
@@ -0,0 +1,11 @@
+
+## ---------------------------
+## Purpose of script: R code for INF4000 data visualisations
+## Author: Ned Blackburn
+## Date Created: 2024-12-11
+
+options(scipen = 6, digits = 5) 
+library(tidyverse)
+library(hrbrthemes)
+
+## ---------------------------
